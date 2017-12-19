aab31b8dc5bc6df8bb723ea5b178b65a1978958e
pbavinck <pbavinck@gmail.com>
2017-12-19 17:26:44 +0100

    Added env variable for testing

--- test.js
+++ test.js
@@ -1,10 +1,12 @@
+require('dotenv').config();
+
 var cloudfunction = require ('./index.js');
 
 //Run local test
 cloudfunction.translateSRTFiles({
   'data': {
     'name': 'sample-en.txt',
-    'bucket': 'cclabs-translate',
+    'bucket': process.env.TEST_BUCKET,
     'resourceState': 'exists',
     'contentType': 'text/plain'
   }
