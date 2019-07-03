fdf956451e1736cfa5d16941502ac79e5b0f27f8
pbavinck <pbavinck@gmail.com>
2019-07-03 10:06:47 +0200

    Fixe tab spacing

--- test.js
+++ test.js
@@ -1,13 +1,13 @@
 require('dotenv').config();
 
-var cloudfunction = require ('./index.js');
+var cloudfunction = require('./index.js');
 
 //Run local test
 cloudfunction.translateSRTFiles({
-  'data': {
-    'name': 'sample-small-en.txt',
-    'bucket': process.env.TEST_BUCKET,
-    'resourceState': 'exists',
-    'contentType': 'text/plain'
-  }
+    data: {
+        name: 'sample-small-en.txt',
+        bucket: process.env.TEST_BUCKET,
+        resourceState: 'exists',
+        contentType: 'text/plain'
+    }
 });
